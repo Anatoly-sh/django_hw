07627e28c62a6d3bcf911876c515d609cf58a692
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-05-10 23:09:15 +0300

    fill.py заполнение БД командой, но с json

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,4 +1,4 @@
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, call_command
 
 from catalog.models import Category, Product
 
@@ -8,33 +8,4 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        category_list = [
-            {'name': 'Юбилейные монеты',
-             'description': 'Монеты, посвященные круглым датам'
-             },
-            {'name': 'Монеты Европы',
-             'description': 'Монеты, выпущенные в Европе'
-             }
-        ]
-        product_list = [
-            {'name': 'Монета 10 рублей 2003 года',
-             'description': 'СПМД «Древние города России — Муром» (Артикул K11-89063)',
-             'category': 'Юбилейные монеты',
-             'purchase_price': 150
-             },
-            {'name': 'Монета 10 рублей 2000 года',
-             'description': 'ММД «55 лет Великой Победы» (Артикул K11-88397)',
-             'category': 'Юбилейные монеты',
-             'purchase_price': 50
-             },
-            {'name': 'Монета 2 евро 2023 года',
-             'description': 'Германия «1275 лет со дня рождения Карла Великого» (Артикул M2-62505)',
-             'category': 'Монеты Европы',
-             'purchase_price': 325
-             },
-            {'name': 'Монета 10 евро 2002 года',
-             'description': 'Испания «XIX зимние Олимпийские Игры 2002 в Солт-Лейк-Сити» (Артикул M2-62501)',
-             'category': 'Монеты Европы',
-             'purchase_price': 3500
-             }
-        ]
+        call_command('loaddata', 'catalog_data.json')   # см. https://django.fun/ru/docs/django/4.1/ref/django-admin/
---
+++ catalog/management/commands/fill_command.py
@@ -0,0 +1,56 @@
+from django.core.management import BaseCommand
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        category_list = [
+            {'name': 'Юбилейные монеты',
+             'description': 'Монеты, посвященные круглым датам'
+             },
+            {'name': 'Монеты Европы',
+             'description': 'Монеты, выпущенные в Европе'
+             }
+        ]
+        product_list = [
+            {'name': 'Монета 10 рублей 2003 года',
+             'description': 'СПМД <Древние города России — Муром> (Артикул K11-89063)',
+             'category': 'Юбилейные монеты',
+             'purchase_price': 150
+             },
+            {'name': 'Монета 10 рублей 2000 года',
+             'description': 'ММД <55 лет Великой Победы> (Артикул K11-88397)',
+             'category': 'Юбилейные монеты',
+             'purchase_price': 50
+             },
+            {'name': 'Монета 2 евро 2023 года',
+             'description': 'Германия «1275 лет со дня рождения Карла Великого» (Артикул M2-62505)',
+             'category': 'Монеты Европы',
+             'purchase_price': 325
+             },
+            {'name': 'Монета 10 евро 2002 года',
+             'description': 'Испания «XIX зимние Олимпийские Игры 2002 в Солт-Лейк-Сити» (Артикул M2-62501)',
+             'category': 'Монеты Европы',
+             'purchase_price': 3500
+             }
+        ]
+
+        category_objects = []
+        for category_item in category_list:
+            category_objects.append(
+                Category(**category_item)
+            )
+
+        Category.objects.bulk_create(category_objects)
+
+        product_objects = []
+        for product_item in product_list:
+            product_objects.append(
+                Product(**product_item)
+            )
+
+        Product.objects.bulk_create(product_objects)
