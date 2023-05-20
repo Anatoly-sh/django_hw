a3c257434d56ac3beceba3c7ae5b1e3bd0b64cc6
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-05-20 22:30:28 +0300

    menu+contacts

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import index, home, contacts
+from catalog.views import index, home, contacts, card, coin
 
 app_name = CatalogConfig.name       # здесь можно: app_name = 'catalog'
 # app_name = 'catalog'
 
 urlpatterns = [
-    path('', index),
+    path('index/', index, name='index'),
     path('home/', home),
     path('contacts/', contacts),
+    path('card/', card),
+    path('coin/<int:pk>/', coin, name='coin_item')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,27 @@
+from pprint import pprint
+
 from django.shortcuts import render
+
+from catalog.models import Product, Category
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    """
+    Отображение домашней страницы
+    """
+    # Формирование параметров каталога из БД
+    num_categories = Category.objects.all().count()
+    num_products = Product.objects.all().count()
+    context = {'num_categories': num_categories,
+               'num_products': num_products,
+               'title': 'Главная'
+               }
+
+    # Отрисовка HTML-шаблона index.html с данными внутри переменной контекста
+    return render(request, 'catalog/index.html', context)
 
 
 def home(request):
@@ -21,4 +38,26 @@ def contacts(request):
               f'с именем: {name}, \n'
               f'электронная почта: {email}, \n'
               f'телефон: {phone}\n')
-    return render(request, 'catalog/contacts.html')
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+def card(request):
+    context = {
+        'category_list': Category.objects.all(),
+        'object_list': Product.objects.all(),
+        'title': 'Список монет'
+    }
+    return render(request, 'catalog/card.html', context)
+
+
+def coin(request, pk):
+    coin_item = Product.objects.get(pk=pk)
+    context = {
+        'object': coin_item,
+        'title': coin_item
+    }
+    return render(request, 'catalog/coin.html', context)
+
